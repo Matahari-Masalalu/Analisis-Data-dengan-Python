e1972b7e2a4681f95ccf2d7a836aac269367638e
Matahari-Masalalu <150545655+Matahari-Masalalu@users.noreply.github.com>
2024-12-19 08:19:13 +0700

    Update dashboard.py

--- Streamlit/dashboard.py
+++ Streamlit/dashboard.py
@@ -120,7 +120,7 @@ def visual_holiday(df_clean_day):
 
     st.pyplot(fig)
 
-df_clean_day = pd.read_csv('https://github.com/Matahari-Masalalu/Analisis-Data-dengan-Python/blob/main/Dataset/data_day.csv', error_bad_lines=False)
+df_clean_day = pd.read_csv("data_day.csv")
 
 date, selected_option = sidebar(df_clean_day)
 df_main = df_clean_day[
